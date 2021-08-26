73ac356e5f30e0f1b3a17e8749f229cba7139b63
SJM <sukjm0915@gmail.com>
2021-08-27 01:08:32 +0900

    selectTitleByAll complete

--- Scripts/3.sql작업.sql
+++ Scripts/3.sql작업.sql
@@ -1,2 +1,11 @@
+select code, name from title;
+  
 select code, name
-  from title;
+  from title 
+where code = 1;
+
+insert into title value (6, '인턴');
+
+delete from title  where code = 6;
+
+update title set name = '계약직' where code = 6;
---
+++ src/java_mysql_erp/dao/TitleDao.java
@@ -0,0 +1,19 @@
+package java_mysql_erp.dao;
+
+import java.util.ArrayList;
+
+import java_mysql_erp.dto.Title;
+
+public interface TitleDao {
+	//select, delete, insert, update
+	ArrayList<Title> selectTitleByAll();
+	
+	Title selectTitleByCode(Title title);
+	
+	int insertTitle (Title title);
+	
+	int updateTitle (Title title);
+	
+	int deleteTitle (Title title);
+	
+}
